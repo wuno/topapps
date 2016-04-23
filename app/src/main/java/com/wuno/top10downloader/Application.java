3bf60157f1eca1cb477506397ae957aa8a08326b
wuno <ndiaz@wuno.com>
2016-04-23 18:07:31 -0500

    Added toString method to dispay the parsed data on in the listView

--- app/src/main/java/com/wuno/top10downloader/Application.java
+++ app/src/main/java/com/wuno/top10downloader/Application.java
@@ -32,4 +32,24 @@ public class Application {
     public void setReleaseDate(String releaseDate) {
         this.releaseDate = releaseDate;
     }
+
+    /**
+     * Returns a string containing a concise, human-readable description of this
+     * object. Subclasses are encouraged to override this method and provide an
+     * implementation that takes into account the object's type and data. The
+     * default implementation is equivalent to the following expression:
+     * <pre>
+     *   getClass().getName() + '@' + Integer.toHexString(hashCode())</pre>
+     * <p>See <a href="{@docRoot}reference/java/lang/Object.html#writing_toString">Writing a useful
+     * {@code toString} method</a>
+     * if you intend implementing your own {@code toString} method.
+     *
+     * @return a printable representation of this object.
+     */
+    @Override
+    public String toString() {
+        return "Name: " + getName() + "\n" +
+                "Artist: " + getArtist() + "\n" +
+                "Release Date: " + getReleaseDate() + "\n";
+    }
 }
